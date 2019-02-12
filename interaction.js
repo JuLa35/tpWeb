270c8ba1babd17f3a68617fc461d88ab51c58df4
JuLa35 <43664937+JuLa35@users.noreply.github.com>
2019-02-12 14:45:23 +0100

    Update interaction.js
    Q1 a 4

--- interaction.js
+++ interaction.js
@@ -2,11 +2,33 @@
 // La création d'un Dnd requière un canvas et un interacteur.
 // L'interacteur viendra dans un second temps donc ne vous en souciez pas au départ.
 function DnD(canvas, interactor) {
-	// Définir ici les attributs de la 'classe'
+	var xInit = 0;
+	var yInit = 0;
+	var xFin = 0;
+	var yFin = 0;
+	
+	var p = x:0,y:0;
 
-	// Developper les 3 fonctions gérant les événements
+	this.Pression = function(evt){
+		this.p = getMousePosition(canvas,evt);
+		this.xInit = this.p.x;
+		this.yInit = this.p.y;
+	}.bind(this);
+	
+	this.Deplacement = function(evt){
+		this.p = getMousePosition(canvas,evt);
+	}.bind(this);
+	
+	this.Relachement = function(evt){
+		this.p = getMousePosition(canvas,evt);
+		this.xFin = this.p.x;
+		this.yFin = this.p.y;
+	}.bind(this);
+	
+	canvas.addEventListener('Drag', this.Pression);
+	canvas.addEventListener('move', this.Deplacement);
+	canvas.addEventListener('Drop', this.Relachement);
 
-	// Associer les fonctions précédentes aux évènements du canvas.
 };
 
 
