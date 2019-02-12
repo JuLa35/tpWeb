8f5916d80bdd0ee2d0f584e9ee57296a04e89969
JuLa35 <43664937+JuLa35@users.noreply.github.com>
2019-02-12 16:01:09 +0100

    Update view.js

--- view.js
+++ view.js
@@ -1,2 +1,50 @@
 
-// Implémenter ici les fonctions paint à ajouter dans chacune des classes du modèle.
+Rectangle.prototype.paint = function(ctx) {
+
+   ctx.fillStyle = '#0040FF';
+
+   ctx.rect(this.getInitX(), this.getInitY(), this.getFinalX(), this.getFinalY());
+
+   ctx.stroke();
+
+};
+
+
+
+Line.prototype.paint = function(ctx) {
+
+   ctx.fillStyle = '#DF0101';
+
+   ctx.beginPath();
+
+   ctx.moveTo(this.getInitX(), this.getInitY());
+
+   ctx.lineTo(this.getFinalX(), this.getFinalY());
+
+   ctx.stroke();
+
+
+
+};
+
+
+
+
+
+Drawing.prototype.paint = function(ctx) {
+
+   console.log(this.getForms());
+
+   ctx.fillStyle = '#F0F0F0'; // set canvas' background color
+
+   ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+   this.getForms().forEach(function(eltDuTableau) {
+
+       // now fill the canvas
+
+       eltDuTableau.paint(ctx);
+
+   });
+
+};
