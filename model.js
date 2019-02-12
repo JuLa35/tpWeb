8392fb4f5417ccfa6ceaeafa1dbe0ca1c4063426
JuLa35 <43664937+JuLa35@users.noreply.github.com>
2019-02-12 15:19:05 +0100

    La vue

--- model.js
+++ model.js
@@ -1,3 +1,28 @@
 
-// Implémenter ici les 4 classes du modèle.
-// N'oubliez pas l'héritage !
+Rectangle.prototype.paint = function(ctx) {
+//TODO Manager color
+   ctx.rect(this.getInitX(), this.getInitY(), this.getFinalX(), this.getFinalY());
+   ctx.stroke();
+};
+
+Line.prototype.paint = function(ctx) {
+//TODO Manager color
+
+   ctx.beginPath();
+   ctx.moveTo(this.getInitX(), this.getInitY());
+   ctx.lineTo(this.getFinalX(), this.getFinalY());
+   ctx.stroke();
+
+};
+
+
+Drawing.prototype.paint = function(ctx) {
+   console.log(this.getForms());
+   ctx.fillStyle = '#F0F0F0'; // set canvas' background color
+   ctx.fillRect(0, 0, canvas.width, canvas.height);
+   this.getForms().forEach(function(eltDuTableau) {
+       // now fill the canvas
+       eltDuTableau.paint(ctx);
+   });
+};
+
